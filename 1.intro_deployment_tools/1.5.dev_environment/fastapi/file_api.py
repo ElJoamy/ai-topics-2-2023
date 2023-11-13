e3b6a1a5b6a0b7bda2238f4b5a361b09aa7bbbcf
Jose Laruta <eduardo.laruta@gmail.com>
2023-11-13 12:39:58 -0400

    train notebook

--- 1.intro_deployment_tools/1.5.dev_environment/fastapi/file_api.py
+++ 1.intro_deployment_tools/1.5.dev_environment/fastapi/file_api.py
@@ -1,6 +1,7 @@
 import io
 import csv
 from fastapi import FastAPI, UploadFile, File, HTTPException, status
+from fastapi.responses import Response
 import numpy as np
 from PIL import Image, UnidentifiedImageError
 
@@ -23,6 +24,30 @@ def upload_image(file: UploadFile = File(...)):
     # crear array de numpy
     img_array = np.array(img_obj)
     return {"filename": file.filename, "image_size": img_array.shape}
+
+@app.get("/reports", responses={
+    200: {"content": {"text/csv": {}}}
+})
+def download_report() -> Response:
+    data = [
+        {"image": "test1.jpg", "score": 0.7},
+        {"image": "test2.jpg", "score": 0.1},
+        {"image": "test3.jpg", "score": 0.99},
+    ]
+    csv_stream = io.StringIO()
+    writer = csv.DictWriter(
+        csv_stream, 
+        fieldnames=["image", "score"],
+        quoting=csv.QUOTE_ALL
+        )
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
+    
+    text = csv_stream.getvalue()
+    return Response(content=text, media_type="text/csv")
+    
+
 
 if __name__ == "__main__":
     import uvicorn
