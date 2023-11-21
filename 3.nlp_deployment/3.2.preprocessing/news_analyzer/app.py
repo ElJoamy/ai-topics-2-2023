93f8182fb435d62f0b6f03e6df5b1c3cfd0f5826
Jose Laruta <eduardo.laruta@gmail.com>
2023-11-21 08:15:57 -0400

    news analyzer

---
+++ 3.nlp_deployment/3.2.preprocessing/news_analyzer/app.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Body, HTTPException, Depends
+import requests
+from functools import cache
+from bs4 import BeautifulSoup
+import spacy
+from pydantic import BaseModel
+
+class ArticleAnalysis(BaseModel):
+    url: str    # url del articulo
+    length: int # longitud en caracteres
+    n_locations: int    # cantidad de ubicaciones mencionadas
+    top_location: str
+    n_people: int       # cantidad de personas mencionadas
+    top_person: str
+    n_org: int
+    top_org: str
+
+
+app = FastAPI(title="News Analyzer")
+
+@cache
+def get_nlp():
+    return spacy.load("es_core_news_md")
+
+@app.post("/news")
+def analyze_news(url: str = Body(...), nlp=Depends(get_nlp)):
+    # un enlace a un articulo
+    # descargar el texto
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise HTTPException(status_code=400, detail="request failed!")
+    # extraer el texto del articulo
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    text_elements = soup.find_all("div", class_="text-editor")
+    texts = [elem.get_text() for elem in text_elements]
+    # procesar el texto
+    doc = nlp(texts[0])
+    # almacenar resultados
+    # retornar resultados
+    return {}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", reload=True)
